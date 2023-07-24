80eb0416e5f4600b5622243d07c459dfd778803d
takoba <plane25@gmail.com>
2023-07-25 04:15:50 +0900

    :construction: Implement interface (but return mock)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-export const serialize = () => {}
+export const serialize = (kana) => {
+  if (kana == 'オハヨウ') return '.-... -... -- ..- '
+}
+export const deserialize = (wabun) => {
+  if (wabun == '.-... -... -- ..- ') return 'オハヨウ'
+}
+export const translate = (input) => {
+  switch (input) {
+    case 'オハヨウ':
+      return '.-... -... -- ..- '
+    case '.-... -... -- ..- ':
+      return 'オハヨウ'
+    default:
+      break
+  }
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,15 +1,51 @@
 import { describe, expect, it } from 'vitest'
-import { serialize } from '../src/index'
+import { deserialize, serialize } from '../src/index'
 
 describe('export const serialize()', () => {
+  it('return wabun when passed kana', () => {
+    // arrange
+    const kana = 'オハヨウ'
+
+    // act
+    const wabun = serialize(kana)
+
+    // assert
+    expect(wabun).toEqual('.-... -... -- ..- ')
+  })
+})
+
+describe('export const deserialize()', () => {
   it('return kana when passed wabun', () => {
     // arrange
     const wabun = '.-... -... -- ..- '
 
     // act
-    const actual = serialize(wabun)
+    const kana = deserialize(wabun)
 
     // assert
-    expect(actual).toEqual('オハヨウ')
+    expect(kana).toEqual('オハヨウ')
+  })
+})
+
+describe('export const translate()', () => {
+  it('return wabun when passed kana', () => {
+    // arrange
+    const kana = 'オハヨウ'
+
+    // act
+    const wabun = serialize(kana)
+
+    // assert
+    expect(wabun).toEqual('.-... -... -- ..- ')
+  })
+  it('return kana when passed wabun', () => {
+    // arrange
+    const wabun = '.-... -... -- ..- '
+
+    // act
+    const kana = deserialize(wabun)
+
+    // assert
+    expect(kana).toEqual('オハヨウ')
   })
 })

739ab30efcafc801f972d656fa3c17d90b13e587
takoba <plane25@gmail.com>
2024-06-28 16:40:47 +0900

    :white_check_mark: Switch test platform (jest -> vitest)

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from 'jest'
-
-const config: Config = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['<rootDir>/test/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
-}
-export default config
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from '@jest/globals'
+import { describe, expect, it } from 'vitest'
 import { deserialize, serialize, translate, wabun as Wabun } from '../src/index'
 
 const examples = [
